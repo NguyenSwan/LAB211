84a95ad56a4f1048aa7486632196cb49f193e6a7
Nguyen Swan <149929360+NguyenSwan@users.noreply.github.com>
2024-06-17 10:17:24 +0700

    Add files via upload

---
+++ StudentManager/src/Model/StudentList.java
@@ -0,0 +1,43 @@
+package Model;
+
+import entity.Student;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentList {
+    private List<Student> sList = new ArrayList<>();
+
+    public List<Student> getStudentList() {
+        return sList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.sList = studentList;
+    }
+    public void addList(Student st){
+        sList.add(st);
+    }
+
+    public void removeList(Student st){
+        sList.remove(st);
+    }
+
+    public Student findById(String id){
+        for(Student st : sList){
+            if(st.getId().equalsIgnoreCase(id)){
+                return st;
+            }
+        }
+        return null;
+    }
+
+    public List<Student> findByName (String name){
+        List<Student> foundList = new ArrayList<>();
+        for (Student st : sList){
+            if(st.getName().toLowerCase().contains(name.toLowerCase())){
+                foundList.add(st);
+            }
+        }
+        return foundList;
+    }
+}
